eb3a4e512037fdd8a308ffbe5257588e4c089ce3
BDtobio <tobiasbustosdiaco989@gmail.com>
2024-09-17 11:32:51 -0300

    primer commit

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+class Activity{
+    constructor(id,title,description,imageUrl){
+    this.id=id;
+    this.title=title;
+    this.description=description;
+    this.imageUrl=imageUrl;
+}
+}
+
+class Repository{
+    constructor(){
+        this.activities=[];
+    }
+ 
+getAllActivities(){
+    return this.activities[i];
+}
+createActivity (){
+
+}
+
+deleteActivity(){
+    
+
+}
+    
+
+
+
+
+
+
+}
+const activity=new Activity(1,"salir a paser", "es sano y divertido","http://img"
+)
+    
